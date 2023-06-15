c5a321e4e91aeea77e7a5bd8268288cf72a1a01d
Gabriel Oliveira <117094724+gabrielolvmd@users.noreply.github.com>
2023-06-15 01:04:34 -0300

    Add files via upload

---
+++ frutifera-js/js/frutifera.js
@@ -0,0 +1,33 @@
+// let frutifera = {
+//   identitificador: '',
+//   nomeEspeciePopular: '',
+//   nomeEspecieCientifico: '',
+//   producaoMediaSafra: 1.5,
+//   dataPlantio: '',
+// };
+
+let frutiferas = [
+  {
+    identificador: '145236842',
+    nomeEspeciePopular: 'Araçá',
+    nomeEspecieCientifico: 'Psidium cattleianum',
+    producaoMediaSafra: 1.5,
+    dataPlantio: '2022-05-12',
+  },
+  {
+    identificador: '1256874239',
+    nomeEspeciePopular: 'Ingá-feijão',
+    nomeEspecieCientifico: 'Inga marginata',
+    producaoMediaSafra: 3,
+    dataPlantio: '2023-03-16',
+  },
+  {
+    identificador: '1225786397',
+    nomeEspeciePopular: 'Pitangueira',
+    nomeEspecieCientifico: 'Eugenia uniflora',
+    producaoMediaSafra: 2.4,
+    dataPlantio: '2022-12-03',
+  },
+];
+
+export default frutiferas;
